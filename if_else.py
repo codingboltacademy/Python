d3657d12c9e99550454f6153a1a97785f722b93a
Coding Bolt <153797446+codingboltacademy@users.noreply.github.com>
2024-08-19 18:47:23 +0530

    Add files via upload

---
+++ if_else.py
@@ -0,0 +1,18 @@
+# marks = float(input("Enter your marks: "))
+# if marks >= 90:
+#     print("Grade: A+ (Outstanding)")
+# elif marks >= 80:
+#     print("Grade: A")
+# elif marks >= 70:
+#     print("Grade: B")
+# elif marks >= 60:
+#     print("Grade: C")
+# else:
+#     print("Grade: F (Fail)")
+
+num = 36
+print ("num = ", num)
+if num % 2 == 0:
+    if num % 3 == 0:
+      print ("Divisible by 3 and 2")
+print("....execution ends....")
